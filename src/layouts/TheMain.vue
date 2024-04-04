35d72be6ce511fb52fbe8185b288320a1941eddf
Lena <bogongE92@gmail.com>
2024-04-04 21:49:44 +0900

    base template 구조 작성

---
+++ src/layouts/TheMain.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+        <TheHeader />
+    </div>
+</template>
+
+<script>
+import TheHeader from './TheHeader.vue'
+export default {
+    components : {
+        TheHeader,
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layouts/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <TheSide></TheSide>
+    <TheMain></TheMain>
+</template>
+
+<script>
+import TheMain from './TheMain.vue';
+import TheSide from './TheSide.vue';
+export default {
+    components:{
+		TheMain,
+        TheSide
+	},
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
